c840d3273a2a8a5317cabe19fe40ae6ec26d0f4e
josassi <johan.sassi@gmail.com>
2024-12-27 22:59:22 +0800

    correct tailwind detection

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4F46E5', // Indigo color from your graph icon
+      },
+    },
   },
   plugins: [],
 };
